2c40b5c45bd5cde7adb44616a1f054ac2afd729b
Sladick <vzalivnov@mail.ru>
2022-01-16 16:57:06 +0300

    1

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from Socket import Socket
 from datetime import datetime
 import asyncio
 from os import system
+import sys
 
 
 class Client(Socket):
@@ -12,22 +13,31 @@ class Client(Socket):
 
     def set_up(self):
         try:
-            self.socket.connect(("127.0.0.1", 2033))
+            self.socket.connect((str(ip_server), int(port)))
         except ConnectionRefusedError:
             print("Server offline")
+            exit(0)
+        except:
             exit(0)
         self.socket.setblocking(False)
 
     async def listen_socket(self, listened_socket=None):
         while True:
-            data = await self.main_loop.sock_recv(self.socket, 2048)
-            self.messages += f'{datetime.today().strftime("%H:%M")} : {data.decode("utf-8")}\n'
-            system("cls")
-            print(self.messages)
+            try:
+                data = await self.main_loop.sock_recv(self.socket, 2048)
+                if not data:
+                    exit(0)
+                self.messages += f'{datetime.today().strftime("%H:%M")} : {data.decode("utf-8")}\r\n'
+                system("cls")
+                print(self.messages)
+            except:
+                print("Close connection: idle timeout expired")
+                exit(0)
 
     async def send_data(self, data=None):
         while True:
             data = await self.main_loop.run_in_executor(None, input, "---->")
+            data += "\r\n"
             await self.main_loop.sock_sendall(self.socket, data.encode("UTF-8"))
 
     async def main(self):
@@ -38,7 +48,11 @@ class Client(Socket):
 
 
 if __name__ == '__main__':
+    try:
+        ip_server, port = sys.argv[1].split(":")  # передача параметров командной строкой
+    except:
+        print("please enter server address")
+        exit(0)
     client = Client()
     client.set_up()
-
     client.start()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from Socket import Socket
-import asyncio
+from datetime import datetime, timedelta
+from socket import SHUT_RDWR
 
 
 class Server(Socket):
@@ -9,16 +10,16 @@ class Server(Socket):
 
     def set_up(self):
         self.socket.bind(("127.0.0.1", 2033))
-        self.socket.listen(5)
+        self.socket.listen(4)  # возможность подключение 4 человек одновременно
         self.socket.setblocking(False)
         print('Server is listening')
 
     async def send_data(self, data=None, listened_socket=None):
         for user in self.users:
-            if user != listened_socket:
+            if user != listened_socket: # дублирование сообщения всем подключенным клиентам (кроме отправителя)
                 await self.main_loop.sock_sendall(user, data)
 
-    async def listen_socket(self, listened_socket=None):
+    async def listen_socket(self, listened_socket=None, start_time=None):
         if not listened_socket:
             return
 
@@ -26,6 +27,13 @@ class Server(Socket):
             try:
                 data = await self.main_loop.sock_recv(listened_socket, 2048)
                 await self.send_data(data, listened_socket)
+                print(f'{datetime.today().strftime("%H:%M")} : {data.decode("utf-8")}')
+                if datetime.now() - start_time > timedelta(seconds=30):  # разрыв соединения если от клиента нет
+                    self.users.remove(listened_socket)  # сообщений более 30 секунд
+                    listened_socket.shutdown(SHUT_RDWR)
+                    listened_socket.close()
+                    return
+                start_time = datetime.now()
             except ConnectionResetError:
                 print("Client removed")
                 self.users.remove(listened_socket)
@@ -36,7 +44,8 @@ class Server(Socket):
             user_socket, address = await self.main_loop.sock_accept(self.socket)
             print(f"User {address[0]} connected")
             self.users.append(user_socket)
-            self.main_loop.create_task(self.listen_socket(user_socket))
+            start_time = datetime.now()
+            self.main_loop.create_task(self.listen_socket(user_socket, start_time))
 
     async def main(self):
         await self.main_loop.create_task(self.accept_sockets())
